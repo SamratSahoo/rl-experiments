73005ec4a7e32a9c0b5b810adcdcc86995d2047a
Samrat Sahoo <samratsahoo2013@gmail.com>
2024-05-13 14:01:53 -0400

    clean up code

--- frozen-lake-q-learning/main.py
+++ frozen-lake-q-learning/main.py
@@ -2,61 +2,59 @@ import gymnasium as gym
 import random
 from operator import itemgetter
 
-env = gym.make("FrozenLake-v1", map_name="4x4", is_slippery=False)
-observation, info = env.reset()
 
-
-q = [[0 for _ in range(4)] for _ in range(16)]
-policy = [-1 for _ in range(4*4)]
-num_action_map = {0: "left", 1: "down", 2: "right", 3: "up", -1: "n/a" }
-
-def get_action(epsilon, current_state):
-    # Choose random action if we haven't set an action yet
-    if policy[current_state] == -1:
-        action = env.action_space.sample()
-    else:
-        # Set policy to be epsilon greedy
-        if random.uniform(0, 1) > 1 - epsilon:
-            action = env.action_space.sample()
-        else:
-            action = policy[current_state]
-
-    return action
-
-def get_adjusted_reward(current_state, next_state, action):
-    bad_states = [5, 7, 11, 12]
-    left_border_states = [0, 4, 8, 12]
-    right_border_states = [3, 7, 11, 15]
-    top_border_states = [0,1,2,3]
-    bottom_border_states = [12,13,14,15]
-
-    if next_state == 15:
-        return 5
-
-    if next_state in bad_states:
-        return -5
-    
-    if num_action_map[action] == "down" and current_state in bottom_border_states:
-        return -2
-    if num_action_map[action] == "up" and current_state in top_border_states:
-        return -2
-    if num_action_map[action] == "left" and current_state in left_border_states:
-        return -2
-    if num_action_map[action] == "right" and current_state in right_border_states:
-        return -2
-
-    return 0
+policy = [-1 for _ in range(4 * 4)]
+num_action_map = {0: "left", 1: "down", 2: "right", 3: "up", -1: "n/a"}
 
 
 def train():
+    env = gym.make("FrozenLake-v1", map_name="4x4", is_slippery=False)
+    current_state, _ = env.reset()
+
+    def get_action(epsilon, current_state):
+        # Choose random action if we haven't set an action yet
+        if policy[current_state] == -1:
+            action = env.action_space.sample()
+        else:
+            # Set policy to be epsilon greedy
+            if random.uniform(0, 1) > 1 - epsilon:
+                action = env.action_space.sample()
+            else:
+                action = policy[current_state]
+
+        return action
+
+    def get_adjusted_reward(current_state, next_state, action):
+        bad_states = [5, 7, 11, 12]
+        left_border_states = [0, 4, 8, 12]
+        right_border_states = [3, 7, 11, 15]
+        top_border_states = [0, 1, 2, 3]
+        bottom_border_states = [12, 13, 14, 15]
+
+        if next_state == 15:
+            return 5
+
+        if next_state in bad_states:
+            return -5
+
+        if num_action_map[action] == "down" and current_state in bottom_border_states:
+            return -2
+        if num_action_map[action] == "up" and current_state in top_border_states:
+            return -2
+        if num_action_map[action] == "left" and current_state in left_border_states:
+            return -2
+        if num_action_map[action] == "right" and current_state in right_border_states:
+            return -2
+
+        return 0
 
     learning_rate = 0.01
     discount_factor = 1
-    current_state = 0
     next_state = 0
     steps = 800
     epsilon = 1
     decay = 0.001
+    q = [[0 for _ in range(4)] for _ in range(16)]
 
     for t in range(1, steps + 1):
         epsilon = max(0, epsilon - decay)
@@ -68,12 +66,9 @@ def train():
         q[current_state][action] = q[current_state][action] + learning_rate * (
             reward
             + discount_factor
-            * max(
-                [q[next_state][possible_action] for possible_action in range(4)]
-            )
+            * max([q[next_state][possible_action] for possible_action in range(4)])
             - q[current_state][action]
         )
-
 
         if terminated or truncated:
             observation, info = env.reset()
@@ -85,16 +80,19 @@ def train():
 
         if t % 100 == 0:
             print("Current Policy:", [num_action_map[p] for p in policy])
-        
+
+
 def simulate():
     terminated = False
     truncated = False
-    env2 = gym.make("FrozenLake-v1", render_mode="human", map_name="4x4", is_slippery=False)
-    current_state, _ = env2.reset()
+    env = gym.make(
+        "FrozenLake-v1", render_mode="human", map_name="4x4", is_slippery=False
+    )
+    current_state, _ = env.reset()
 
     while not terminated and not truncated:
         action = policy[current_state]
-        next_state, _, terminated, truncated, _ = env2.step(action)
+        next_state, _, terminated, truncated, _ = env.step(action)
         current_state = next_state
 
     if truncated:
@@ -107,6 +105,7 @@ def simulate():
 
     env.close()
 
+
 def evaluate():
     passed = 0
     failed = 0
@@ -114,12 +113,12 @@ def evaluate():
     for i in range(0, 10000):
         terminated = False
         truncated = False
-        env2 = gym.make("FrozenLake-v1", map_name="4x4", is_slippery=False)
-        current_state, _ = env2.reset()
+        env = gym.make("FrozenLake-v1", map_name="4x4", is_slippery=False)
+        current_state, _ = env.reset()
 
         while not terminated and not truncated:
             action = policy[current_state]
-            next_state, _, terminated, truncated, _ = env2.step(action)
+            next_state, _, terminated, truncated, _ = env.step(action)
             current_state = next_state
 
         if truncated:
@@ -130,8 +129,8 @@ def evaluate():
             else:
                 failed += 1
 
-        env.close()
-    print (f"Success Rate = {100*passed/(passed + failed)}%")
+    env.close()
+    print(f"Success Rate = {100*passed/(passed + failed)}%")
 
 
 if __name__ == "__main__":
