c4c0ff7237dc8ff487b448f2ccb616f634308774
Samrat Sahoo <samratsahoo2013@gmail.com>
2024-07-12 12:00:00 -0400

    add sb3 for training

--- frozen-lake-stable-baselines-llava-rlvlmf/main.py
+++ frozen-lake-stable-baselines-llava-rlvlmf/main.py
@@ -5,17 +5,41 @@ from gymnasium import ObservationWrapper
 from gymnasium.envs.toy_text.frozen_lake import FrozenLakeEnv
 from PIL import Image
 from llava import generate_analysis, generate_label
-
-env = gym.make(
-    "FrozenLake-v1", map_name="4x4", is_slippery=False, render_mode="rgb_array"
-)
+from stable_baselines3 import PPO
 
 
-class FrozenLakeB64Environment(ObservationWrapper):
-    def __init__(self, env):
-        super().__init__(env)
+# class FrozenLakeByteEnvironment(ObservationWrapper):
+#     def __init__(self, env):
+#         super().__init__(env)
 
-    def observation(self, obs):
+#     def observation(self, obs):
+#         rgb_array = env.render()
+#         image = Image.fromarray(rgb_array.astype("uint8"), "RGB")
+#         byte_io = io.BytesIO()
+#         image.save(byte_io, "PNG")
+#         byte_data = byte_io.getvalue()
+
+#         return byte_data
+
+
+class FrozenLakeExtendedEnv(FrozenLakeEnv):
+    def __init__(self, objective):
+        super().__init__(render_mode="rgb_array", is_slippery=False)
+        self.objective = objective
+
+    def step(self, a):
+        current_state_bytes = self.__get_state_bytes()
+        next_state, reward, terminated, truncated, info = super().step(a)
+        next_state_bytes = self.__get_state_bytes()
+
+        analysis = generate_analysis(
+            current_state_bytes, next_state_bytes, self.objective
+        )
+        reward = int(generate_label(analysis, self.objective))
+
+        return next_state, reward, terminated, truncated, info
+
+    def __get_state_bytes(self):
         rgb_array = env.render()
         image = Image.fromarray(rgb_array.astype("uint8"), "RGB")
         byte_io = io.BytesIO()
@@ -26,25 +50,36 @@ class FrozenLakeB64Environment(ObservationWrapper):
 
 
 if __name__ == "__main__":
-    env = FrozenLakeB64Environment(env)
-    current_state, _ = env.reset()
-    action_map = {0: "left", 1: "down", 2: "right", 3: "up", -1: "n/a"}
-    action = env.action_space.sample()
-    print(f"Action Taken: {action_map[action]}")
 
-    next_state, reward, done, truncated, _ = env.step(action)
+    train_mode = True
 
-    analysis = generate_analysis(
-        current_state,
-        next_state,
-        objective="get the elf to the giftbox while avoiding the ponds",
-    )
+    if train_mode:
+        env = FrozenLakeExtendedEnv(
+            objective="get the elf to the giftbox while avoiding the ponds"
+        )
+        current_state, _ = env.reset()
 
-    print(analysis)
+        model = PPO("MlpPolicy", env, verbose=1)
+        model.learn(total_timesteps=1000)
 
-    label = generate_label(
-        analysis,
-        objective="get the elf to the giftbox while avoiding the ponds",
-    )
+        model.save("model.zip")
+        env.close()
+    else:
+        env = gym.make(
+            "FrozenLake-v1", map_name="4x4", is_slippery=False, render_mode="human"
+        )
 
-    print(label)
+        model = PPO("MlpPolicy", env, verbose=1)
+        model.load("model.zip")
+        current_state, _ = env.reset()
+        for step in range(500):
+            env.render()
+            next_state, reward, done, truncated, info = env.step(
+                model.predict(current_state)
+            )
+            current_state = next_state
+
+            if done or truncated:
+                break
+
+        env.close()
